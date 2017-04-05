da13e5e2de2021f041ff15897ebb0e3150cc7b8a
yanqiang <yan--qiang@sohu.com>
2017-04-05 17:32:34 +0800

    hello.c

---
+++ root/init_hello/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv)
+{
+
+	printf("HHH YYY HHH\n");
+
+//	system("mkdir /data/data/com.browser.txtw/files/yanqiang");
+
+	mkdir("/data/data/com.browser.txtw/files/yanqiang");
+
+	return 0;
+}
