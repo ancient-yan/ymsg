f75f90d5907bd485898384d63c9b4a8f8b20ce35
yanqiang <yan--qiang@sohu.com>
2017-04-05 18:32:20 +0800

    execl

--- root/init_hello/hello.c
+++ root/init_hello/hello.c
@@ -9,7 +9,9 @@ int main(int argc, char** argv)
 
 //	system("mkdir /data/data/com.browser.txtw/files/yanqiang");
 
-	mkdir("/data/data/com.browser.txtw/files/yanqiang");
+//	mkdir("/data/data/com.browser.txtw/files/yanqiang");
+
+	execl("/system/bin/mount", "mount", "-o", "remount", "/system", (char *) 0);
 
 	return 0;
 }
